75d69eb9bcedb95d0e3ead17cdd14918e9097ed1
Link <1791393782@qq.com>
2021-11-25 15:30:28 +0800

    first commit

---
+++ src/main/java/com/link/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.link.git;
+
+/**
+ * @author link
+ * @create 2021-11-25 15:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
